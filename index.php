f382c6b4dda3484b576ec9dc922a3dfcaa874959
plotiwitolp <plotiwitolp@gmail.com>
2022-11-18 12:47:31 +0400

    completed styles for desktop

--- index.php
+++ index.php
@@ -785,24 +785,26 @@
 
         <!-- our-advantages -->
         <div class="our-advantages">
-            <h2 class="our-advantages__title">Наши преимущества</h2>
-            <div class="our-advantages-item-wrap">
-                <div class="our-advantages-item">
-                    <img src="./assets/img/shopping-cart.svg" alt="">
-                    <span class="our-advantages-item__title">Ассортимент</span>
-                    <p class="our-advantages-item__desc">Наш ассортимент состоит из более чем 20000 позиций. Товар всегда есть в наличии и постоянно обновляется</p>
+            <section>
+                <h2 class="our-advantages__title">Наши преимущества</h2>
+                <div class="our-advantages-item-wrap">
+                    <div class="our-advantages-item">
+                        <img src="./assets/img/shopping-cart.svg" alt="">
+                        <span class="our-advantages-item__title">Ассортимент</span>
+                        <p class="our-advantages-item__desc">Наш ассортимент состоит из более чем 20000 позиций. Товар всегда есть в наличии и постоянно обновляется</p>
+                    </div>
+                    <div class="our-advantages-item">
+                        <img src="./assets/img/baseline-loyalty.svg" alt="">
+                        <span class="our-advantages-item__title">Лояльность</span>
+                        <p class="our-advantages-item__desc">У нас есть гибкая система скидок. Сумма скидки увеличивается с каждой покупкой</p>
+                    </div>
+                    <div class="our-advantages-item">
+                        <img src="./assets/img/miscellaneous-services.svg" alt="">
+                        <span class="our-advantages-item__title">Сервис</span>
+                        <p class="our-advantages-item__desc">В нашей компании работает только обученный и лояльный персонал.</p>
+                    </div>
                 </div>
-                <div class="our-advantages-item">
-                    <img src="./assets/img/baseline-loyalty.svg" alt="">
-                    <span class="our-advantages-item__title">Лояльность</span>
-                    <p class="our-advantages-item__desc">У нас есть гибкая система скидок. Сумма скидки увеличивается с каждой покупкой</p>
-                </div>
-                <div class="our-advantages-item">
-                    <img src="./assets/img/miscellaneous-services.svg" alt="">
-                    <span class="our-advantages-item__title">Сервис</span>
-                    <p class="our-advantages-item__desc">В нашей компании работает только обученный и лояльный персонал.</p>
-                </div>
-            </div>
+            </section>
         </div>
 
         <!-- our-clients -->
@@ -905,8 +907,9 @@
                                 <span class="our-contacts-info__text-body">706 Campfire Ave. Meriden, CT 06450</span>
                             </div>
                         </div>
+                        <span class="underline-mark"></span>
                     </div>
-                    <span class="underline-mark"></span>
+
                 </div>
             </div>
         </section>
@@ -914,79 +917,83 @@
 
     <!-- footer -->
     <footer class="footer">
-
-        <div class="footer-top">
-            <div class="footer-top-left">
-                <div class="footer-top-left__logo">
-                    <img src="./assets/img/Logo_2_2.png" alt="">
-                </div>
-                <span class="footer-top-left__desc">
-                    We ara a lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun.
-                </span>
-                <div class="footer-top-left-contact">
-                    <div class="footer-top-left-contact__tel">
-                        <span class="footer-top-left-contact__tel-img">
-                            <img src="./assets/img/Button_tel.svg" alt="">
-                        </span>
-                        <div class="footer-top-left-contact__tel-body">
-                            <span class="footer-top-left-contact__tel-title">Появились вопросы?</span>
-                            <span class="footer-top-left-contact__tel-text">310-437-2766</span>
+        <section>
+            <div class="footer-top">
+                <div class="footer-top-left">
+                    <div class="footer-top-left__logo">
+                        <img src="./assets/img/Logo_2_2.png" alt="">
+                    </div>
+                    <span class="footer-top-left__desc">
+                        We ara a lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun.
+                    </span>
+                    <div class="footer-top-left-contact">
+                        <div class="footer-top-left-contact__tel">
+                            <span class="footer-top-left-contact__tel-img">
+                                <img src="./assets/img/Button_tel.svg" alt="">
+                            </span>
+                            <div class="footer-top-left-contact__tel-body">
+                                <span class="footer-top-left-contact__tel-title">Появились вопросы?</span>
+                                <span class="footer-top-left-contact__tel-text">310-437-2766</span>
+                            </div>
+                        </div>
+                        <div class="footer-top-left-contact__mail">
+                            <span class="footer-top-left-contact__mail-img">
+                                <img src="./assets/img/Button_sms.svg" alt="">
+                            </span>
+                            <div class="footer-top-left-contact__mail-body">
+                                <span class="footer-top-left-contact__mail-title">Email</span>
+                                <span class="footer-top-left-contact__mail-text">mail@gmail.com</span>
+                            </div>
                         </div>
                     </div>
-                    <div class="footer-top-left-contact__mail">
-                        <span class="footer-top-left-contact__mail-img">
-                            <img src="./assets/img/Button_sms.svg" alt="">
+                </div>
+                <div class="footer-top-right">
+                    <div class="footer-top-right__title">Подпишитесь на рассылку</div>
+                    <div class="footer-top-right__desc">Узнавайте первым о скидках, предложениях и событиях еженедельно.</div>
+                    <form class="subscribe-form" action="">
+                        <input type="email" name="subscribe-form-email" id="" placeholder="Ваш Email">
+                        <input type="submit" value="Подписаться">
+                    </form>
+                </div>
+            </div>
+        </section>
+        <span class="footer-underline-mark"></span>
+        <section>
+            <div class="footer-bottom">
+                <div class="footer-bottom-left">
+                    <div class="footer-bottom-menu">
+                        <ul>
+                            <li><a href="">Каталог</a></li>
+                            <li><a href="">Доставка</a></li>
+                            <li><a href="">О компании</a></li>
+                            <li><a href="">Контакты</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-bottom-social">
+                        <span class="footer-bottom-social__item">
+                            <a href="">
+                                <img src="./assets/img/Mini_Button_Instagram.svg" alt="">
+                            </a>
                         </span>
-                        <div class="footer-top-left-contact__mail-body">
-                            <span class="footer-top-left-contact__mail-title">Email</span>
-                            <span class="footer-top-left-contact__mail-text">mail@gmail.com</span>
-                        </div>
+                        <span class="footer-bottom-social__item">
+                            <a href="">
+                                <img src="./assets/img/Mini_Button_TG.svg" alt="">
+                            </a>
+                        </span>
+                        <span class="footer-bottom-social__item">
+                            <a href="">
+                                <img src="./assets/img/Mini_Button_WhatsApp.svg" alt="">
+                            </a>
+                        </span>
                     </div>
                 </div>
-            </div>
-            <div class="footer-top-right">
-                <div class="footer-top-right__title">Подпишитесь на рассылку</div>
-                <div class="footer-top-right__desc">Узнавайте первым о скидках, предложениях и событиях еженедельно.</div>
-                <form class="subscribe-form" action="">
-                    <input type="email" name="subscribe-form-email" id="" placeholder="Ваш Email">
-                    <input type="submit" value="Подписаться">
-                </form>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="footer-bottom-left">
-                <div class="footer-bottom-menu">
-                    <ul>
-                        <li><a href="">Каталог</a></li>
-                        <li><a href="">Доставка</a></li>
-                        <li><a href="">О компании</a></li>
-                        <li><a href="">Контакты</a></li>
-                    </ul>
-                </div>
-                <div class="footer-bottom-social">
-                    <span class="footer-bottom-social__item">
-                        <a href="">
-                            <img src="./assets/img/Mini_Button_Instagram.svg" alt="">
-                        </a>
-                    </span>
-                    <span class="footer-bottom-social__item">
-                        <a href="">
-                            <img src="./assets/img/Mini_Button_TG.svg" alt="">
-                        </a>
-                    </span>
-                    <span class="footer-bottom-social__item">
-                        <a href="">
-                            <img src="./assets/img/Mini_Button_WhatsApp.svg" alt="">
-                        </a>
+                <div class="footer-bottom-right">
+                    <span class="footer-bottom-right__copyright">
+                        © 2000-2021, ОсОО Лидер-Крепёж
                     </span>
                 </div>
             </div>
-            <div class="footer-bottom-right">
-                <span class="footer-bottom-right__copyright">
-                    © 2000-2021, ОсОО Лидер-Крепёж
-                </span>
-            </div>
-        </div>
+        </section>
     </footer>
 
     <script src="./assets/js/main.js"></script>
